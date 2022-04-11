d20fba1bd5dab175fd0003006cd19bac5464c195
Berat Yavuz <beratttyavuzz@gmail.com>
2022-04-11 13:14:44 +0300

    Initial Commit

---
+++ SwiftUIdeneme/SwiftUIdenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIdenemeApp.swift
+//  SwiftUIdeneme
+//
+//  Created by Berat Yavuz on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIdenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
