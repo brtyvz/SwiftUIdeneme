c787807ff47be7535a065272d378736e7d4f7002
Berat Yavuz <beratttyavuzz@gmail.com>
2022-04-11 14:18:21 +0300

    commit

--- SwiftUIdeneme/ContentView.swift
+++ SwiftUIdeneme/ContentView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+class Veri:ObservableObject{
+    
+    @Published var sayac : Int = 0
+    
+}
 
 struct ContentView: View {
+    @ObservedObject var gelenVeri = Veri()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        VStack{
+            Text("sayı:\(gelenVeri.sayac)")
+            Button(action: {gelenVeri.sayac+=1}, label: {
+                Text("Arttır")
+                
+                NavigationLink(
+                    destination: ContentView2(sonveri: self.$gelenVeri.sayac),
+                    label: {
+                        Text("Navigate")
+                    })
+            })
+            
+        }
+        }.navigationTitle( "ilk sayfa" )
     }
 }
+
+struct ContentView2: View {
+    @Binding var sonveri : Int
+    var body: some View {
+        VStack{
+            Text("sonveri\(sonveri)")
+            Button(action: {self.sonveri += 1}) {
+                Text("Button arttır")
+            }
+            
+        }.navigationTitle("ikinci sayfa")
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
